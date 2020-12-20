f3d7a2093abe67ab8f3bc7f62304cbc46d1a958b
edwvil <efvillar@unal.edu.co>
2020-12-20 10:50:08 -0500

    utro

--- db1.py
+++ db1.py
@@ -12,12 +12,12 @@ class Transaccion_I(BaseModel):
 
     
 transaciones_I = {
-    1: Transaccion_I (id=1 , fechaTransaccion = "01-11-2020", categoria="salario",valor=2000000, formaPago="tarjeta_débito", descripcion= "Quincena 1"),
-    2: Transaccion_I (id=2 , fechaTransaccion = "08-11-2020", categoria="prima",valor=1200000, formaPago="tajerta_crédito", descripcion= "Prima"),
-    3: Transaccion_I (id=3 , fechaTransaccion = "15-12-2020", categoria="salario",valor=2000000, formaPago="efectivo", descripcion= "Quincena 2"),
+    1: Transaccion_I (id=1 , fechaTransaccion = "01-11-2020", categoria="salario",valor=2000000, formaPago="transferencia", descripcion= "Quincena 1"),
+    2: Transaccion_I (id=2 , fechaTransaccion = "08-11-2020", categoria="prima",valor=1200000, formaPago="transferencia", descripcion= "Prima"),
+    3: Transaccion_I (id=3 , fechaTransaccion = "15-12-2020", categoria="salario",valor=2000000, formaPago="transferencia", descripcion= "Quincena 2"),
 }
 
-"""
+
 def obtener_Transaciones_I():
     #haga lo que tenga que hacer para conectarse a la base de datos y obtner todas las ordenes
     lista_transacciones_I=[]
@@ -25,7 +25,7 @@ def obtener_Transaciones_I():
         lista_transacciones_I.append(transaciones_I[i])
     return lista_transacciones_I
 
-
+"""
 def agregar_transaccion_I(transaccion: Transaccion_I):
     if transaccion.id in transaciones_I:
         return False
