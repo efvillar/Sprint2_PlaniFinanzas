f7ac6936c4e946a23bb754e395a2b3ffb044e43d
edwvil <efvillar@unal.edu.co>
2020-12-20 10:35:12 -0500

    X+17

--- db1.py
+++ db1.py
@@ -10,14 +10,14 @@ class Transaccion_I(BaseModel):
     descripcion_I: str 
 
 
-"""
+
 transaciones_I = {
     1: Transaccion_I (id_I=3 , fechaTransaccion_I = "01-12-2020", categoria_I="salario",valor_I=2000000,  descripcion= "Quincena 1"),
     2: Transaccion_I (id_I=2 , fechaTransaccion_I = "08-12-2020", categoria_I="prima",valor_I=1200000,  descripcion= "Prima de Diciembre"),
     3: Transaccion_I (id_I=3 , fechaTransaccion_I = "15-12-2020", categoria_I="salario",valor_I=2000000,  descripcion= "Quincena 2"),
 }
 
-
+"""
 def obtener_Transaciones_I():
     #haga lo que tenga que hacer para conectarse a la base de datos y obtner todas las ordenes
     lista_transacciones_I=[]
